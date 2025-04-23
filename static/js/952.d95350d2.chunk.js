7a92a89cb3e26e6ead1c6911c2173e8b65ac68f9
gal mir <galmirr@gmail.com>
2025-04-23 16:40:25 +0300

    Updates

---
+++ static/js/952.d95350d2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdina=self.webpackChunkdina||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.d95350d2.chunk.js.map
